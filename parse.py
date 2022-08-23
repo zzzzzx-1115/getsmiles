15adc8c87c1159eddc16ca7d52167a849ad5da01
zzzzzx-1115 <xinzheng1115@gmail.com>
2022-08-23 12:22:09 +0800

    112

--- parse.py
+++ parse.py
@@ -3,6 +3,6 @@
 
 
 if __name__ == '__main__':
-    lines = [line.strip("\r\n ").split(' ')[0] for line in open('chembl_origin.txt')]
+    lines = [line.strip("\r\n ").split(' ')[0] for line in open('refined_origin.txt')]
     for line in set(lines):
         print(line)
